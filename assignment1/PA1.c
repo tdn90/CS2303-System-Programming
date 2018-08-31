695666d2f4224f07d2588a922ceddbd04bb1f25b
tdn90 <tnguyen4@wpi.edu>
2018-08-31 16:32:05 -0400

    Add JavaDoc comments

--- assignment1/PA1.c
+++ assignment1/PA1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #define JAN 1
 #define FEB 2
 #define MAR 3
@@ -20,28 +19,45 @@ void printLine();
 int checkLeapYear(int year);
 int getJan1(int year);
 
+/**
+ * Print out a 12-month Gregorian Calendar.
+ * @return 0 if the program execute normally, 1 for early exit.
+ * @author: Dung (Kevin) Nguyen
+ */
 int main() {
 	printf("\t\tMONTHLY CALENDAR\n\n");
+
+	// Ask for input year
 	int year;
 	printf("Please enter year for this calendar: ");
 	scanf("%d", &year);
-	printf("\n");
+	printf("\n\n");
 
+	// Error checking
 	if (year <= 1582) {
 		printf("Invalid year number for Gregorian Calendar.\n");
 		return 1;
 	}
 
-	printf("* * *\t\tCALENDAR for %d\t\t* * *\n\n", year);
+	printf("***     CALENDAR for %d     ***\n\n", year);
 	
+	// Keep track of the first day of each month 	
 	int cur_month_start_date = getJan1(year);
+
+	// Print each month
 	for (int i = JAN; i <= DEC; i++) {
 		printLine();
-		printf("\t\t\t");
+
+		// Print current month's name and get current month's total days
 		int cur_month_days = get_days_print_month(i);
+		printf(" %d", year);
+
+		// Add another day if leap year
 		if (i == FEB) cur_month_days += checkLeapYear(year);
 		printf("\n\n");
 		printDays();
+
+		// print every days in given month
 		cur_month_start_date = printDaysInMonth(cur_month_days, cur_month_start_date);		
 	}
 	return 0;
@@ -49,14 +65,15 @@ int main() {
 
 /**
  * Determine the start date of the year
- * Day code:
- * Sunday: 0
- * Monday: 1
- * Tuesday: 2
- * Wednesday: 3
- * Thursday: 4
- * Friday: 5
- * Saturday: 6
+ * Day code table:
+ * +-----+-----+-----+-----+-----+-----+-----+
+   | Sun | Mon | Tue | Wed | Thu | Fri | Sat |
+   +-----+-----+-----+-----+-----+-----+-----+
+   |   0 |   1 |   2 |   3 |   4 |   5 |   6 |
+   +-----+-----+-----+-----+-----+-----+-----+
+ * @param year Given year (after 1582)
+ * @return: the day code of January 1st of the given year
+ * @author: Dung (Kevin) Nguyen
  */
 int getJan1(int year) {
 	int num_day, zeller_month, year_code, century; 
@@ -71,7 +88,9 @@ int getJan1(int year) {
 
 /**
  * Determine whether the given year is a leap year.
- * If it is a leap year, return 1 so we can add one more day to February. Else return 0.
+ * @param year Given year (after 1582 exclusive)
+ * @return 1 if it is a leap year, 0 otherwise.
+ * @author: Dung (Kevin) Nguyen
  */
 int checkLeapYear(int year) {
 	if (year % 4 == 0) {
@@ -84,17 +103,29 @@ int checkLeapYear(int year) {
 	else return 0;
 }
 
-// Make the calendar look more organized
+/** 
+ * Print out a horizontal line, make the calendar look more organized
+ * @author: Dung (Kevin) Nguyen
+ */
 void printLine() {
-	for (int i = 0; i < 56; i++) printf("-");
+	for (int i = 0; i < 34; i++) printf("-");
 	printf("\n");
 }
 
+/**
+ * Print out days' name in the week
+ * @author: Dung (Kevin) Nguyen
+ */ 
 void printDays() {
-	printf("Sun\tMon\tTue\tWed\tThu\tFri\tSat\n");
+	printf("Sun  Mon  Tue  Wed  Thu  Fri  Sat\n");
 }
 
-// Print the month and return the days that the given month has
+/**
+ * Print out the current month's name
+ * @param month_code Given month_code to determine the current month. Follows the pattern: 1-12 --- Jan-Dec
+ * @return number of days in the given month
+ * @author: Dung (Kevin) Nguyen
+ */ 
 int get_days_print_month(int month_code) {
 	switch(month_code) {
 		case JAN: printf("January");
@@ -126,26 +157,32 @@ int get_days_print_month(int month_code) {
 }
 
 /**
- * Given the total day in a month and the start day 
- * Print out all days of that month
- * And return the start day of next month 
- */
+ * Print out the calendar of the given month
+ * @param totay_day: given month's number of days
+ * @param start_day: given month's start date, following day_code table
+ * @return the start date of the following month.
+ * @author: Dung (Kevin) Nguyen
+ */ 
 int printDaysInMonth(int total_day, int start_day) {
-	int start = 0;
+	// Customize spacing
+	if (start_day > 0) printf("   ");
+	int start = 1;
 	while (start < start_day) {	
-		printf(" \t");
+		printf("     ");
 		start++;
 	}
-
-	int current_day = 1;
+	
+	int current_day = 1; 
+	// print out the days in month
 	while (current_day <= total_day) {
-		printf("%d\t",current_day);
-
-		if ((current_day + start_day - 1) % 7 == 6) printf("\n");
-
+		int day_code = (current_day + start_day - 1) % 7;
+		if(day_code == 0) printf("%3d", current_day);
+		else printf("%5d",current_day);
+		if (day_code == 6) printf("\n");
 		current_day++;
 	}
-
+	
+	// Get start date of following month 
 	int next_start = (start_day + total_day) % 7;
 	if (next_start != 0) printf("\n");
 	printf("\n");
