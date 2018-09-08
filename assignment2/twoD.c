e44d313652bffd42ec0b5c924a55d8e96c8fb372
tdn90 <tnguyen4@wpi.edu>
2018-09-08 18:01:53 -0400

    Remove sample make2DInt function

--- assignment2/twoD.c
+++ assignment2/twoD.c
@@ -2,40 +2,6 @@
 #include <stdlib.h>
 #include "twoD.h"
 
-// TODO Remove afterwards
-/** Make a 2D array of integers (this is an example)
- *
- * @param nrows Number of rows
- * @param ncolumns Number of columns
- * @return Pointer to the array of pointers to the rows.
- * 	  or null pointer if unable to allocate memory.
- * 	  Note: Will not free partially-allocated memory.
- *
- * @author Mike Ciaraldi
- */
-
-int** make2Dint(int nrows, int ncolumns) {
-
-	int **a; // Array of pointers to rows
-	unsigned int i; // Loop counter
-
-	// First allocate the array of pointers to rows
-	a = (int **) malloc(nrows * sizeof(int *));
-	if (!a) { // Unable to allocate the array
-		return (int **) NULL;
-	}
-
-	// Now allocate array for each row
-	for (i = 0; i < nrows; i++) {
-		// i is the row we are about to allocate
-		a[i] = malloc(ncolumns * sizeof (int));
-		if (!a[i]) {
-			return (int **) NULL; // Unable to allocate
-		}
-	}
-	return a;
-}
-
 /** Make a 2D array of characters
  *
  * @param nrows Number of rows
@@ -106,6 +72,15 @@ int getNeighbors(char **array, int nrows, int ncolumns, int currentR, int curren
 	return neighbors;
 }
 
+/**
+ * Helper function for getNeighbors
+ * Get a specific value of the cell.
+ * @array given grid
+ * @nrows number of rows
+ * @columns number of columns
+ * @row current row
+ * @col current column
+ */
 int getCellVal(char **array, int nrows, int ncolumns, int row, int col) {
 	if (row >= 0 && row < nrows && col >= 0 && col < ncolumns) {
 		return *(*(array + row) + col) == 'x' ? 1 : 0;
