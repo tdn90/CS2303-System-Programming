cb52741849dcedb89ac34188ba86891e9b9329b3
tdn90 <tnguyen4@wpi.edu>
2018-09-08 19:33:50 -0400

    Add new functions' headers from twoD.c

--- assignment2/twoD.h
+++ assignment2/twoD.h
@@ -8,7 +8,15 @@
 #define TWOD_H_
 
 // Function prototypes:
-int** make2Dint(int nrows, int ncolumns);
 char** make2Dchar(int nrows, int ncolumns);
+void print2DArray(char **array, int nrows, int ncolumns);
+int getNeighbors(char **array, int nrows, int ncolumns, int currentR, int currentC);
+int getCellVal(char **array, int nrows, int ncolumns, int row, int col);
+void fillInitGrid(char **array, int nrows, int ncolumns, char initVal);
+void setGridFromFile(char **array, int nrows, int ncolumns, FILE *fp);
+int getMaxLineLength(FILE *fp);
+int getNumLines(FILE *fp);
+void fillUpGrid(char **array, int nrows, int ncolumns);
+void centerGrid(char **original, char **centered, int nrows, int ncolumns, int maxWidth, int maxHeight);
 
 #endif /* 2D_H_ */
