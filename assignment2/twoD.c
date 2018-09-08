ef19d60f6b7ab2109c2f64d56086729564e349be
tdn90 <tnguyen4@wpi.edu>
2018-09-08 18:09:24 -0400

    Implement get longest line

--- assignment2/twoD.c
+++ assignment2/twoD.c
@@ -132,3 +132,26 @@ void setGridFromFile(char **array, int nrows, int ncolumns, FILE *fp) {
 	} while (curRow < nrows && curCol <= ncolumns);
 }
 
+/**
+ * Get the longest line's length in the given file
+ * @fp the pointer to a given file
+ */
+int getMaxLineLength(FILE *fp) {
+	int c;
+	int currentLen;
+	int max = 0;
+
+	do {
+		c = fgetc(fp);
+		if (feof(fp))
+			break;
+		if (c == '\n') {
+			if (currentLen > max) max = currentLen;
+			currentLen = 0;
+		} else {
+			 currentLen++;
+		}
+	} while (1);
+	return max;
+}
+
