b30f39db1908aa0e551b876e3092069db03b79c0
tdn90 <tnguyen4@wpi.edu>
2018-09-09 14:03:57 -0400

    Complete header file for twoD

--- assignment2/twoD.h
+++ assignment2/twoD.h
@@ -1,22 +1,17 @@
 /**
  * twoD.h
  *
- * @author: Mike Ciaraldi
+ * @author: Dung (Kevin) Nguyen
  */
 
 #ifndef TWOD_H_
 #define TWOD_H_
 
-// Function prototypes:
 char** make2Dchar(int nrows, int ncolumns);
+void free2DGrid(char **array, int nrows);
 void print2DArray(char **array, int nrows, int ncolumns);
-int getNeighbors(char **array, int nrows, int ncolumns, int currentR, int currentC);
+int getNeighbors(char **array, int nrows, int ncolumns, int currentR,
+		int currentC);
 int getCellVal(char **array, int nrows, int ncolumns, int row, int col);
-void fillInitGrid(char **array, int nrows, int ncolumns, char initVal);
-void setGridFromFile(char **array, int nrows, int ncolumns, FILE *fp);
-int getMaxLineLength(FILE *fp);
-int getNumLines(FILE *fp);
-void fillUpGrid(char **array, int nrows, int ncolumns);
-void centerGrid(char **original, char **centered, int nrows, int ncolumns, int maxWidth, int maxHeight);
 
 #endif /* 2D_H_ */
