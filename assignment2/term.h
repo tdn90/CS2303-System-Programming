da1f03a3aca84a6ec2b2068bb05441579fc0bf6d
tdn90 <tnguyen4@wpi.edu>
2018-09-09 14:05:10 -0400

    Complete header file for term

---
+++ assignment2/term.h
@@ -0,0 +1,18 @@
+/**
+ * TERM.h
+ *
+ * @author: Dung (Kevin) Nguyen
+ */
+
+#ifndef TERM_H_
+#define TERM_H_
+
+#define EXIT_CODE_1 1
+#define EXIT_CODE_2 2
+#define EXIT_CODE_3 3
+
+int equalsGrid(char **first, char **second, int nrows, int ncolumns);
+int allDead(char **array, int nrows, int ncolumns);
+
+#endif /* TERM_H_ */
+
