d744dc3f811991db9b5119912b6a5da98f5f1af4
tdn90 <tnguyen4@wpi.edu>
2018-09-28 18:16:11 -0400

    Add initial files to set up the project

---
+++ PA5_tnguyen4/qSim.cpp
@@ -0,0 +1,16 @@
+/*
+ * qSim.cpp
+ *
+ *  Created on: Sep 28, 2018
+ *      Author: tnguyen4
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "Hello World" << endl;
+	return 1;
+}
+
