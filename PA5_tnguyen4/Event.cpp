32f5761d10af37ddfb1d02afc590345d2c478e97
tdn90 <tnguyen4@wpi.edu>
2018-10-11 19:57:37 -0400

    Complete documentation for Event

--- PA5_tnguyen4/Event.cpp
+++ PA5_tnguyen4/Event.cpp
@@ -6,10 +6,17 @@
 
 #include "Event.h"
 
+/**
+ * Constructor
+ * @param t Time (in minutes) that this event happens
+ */
 Event::Event(double t) {
 	this->time = t;
 }
 
+/**
+ * @return Time that this event happens
+ */
 double Event::getTime() {
 	return this->time;
 }
