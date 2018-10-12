873a8456f1c2b25a52d661ec742f0e6690733638
tdn90 <tnguyen4@wpi.edu>
2018-10-11 20:25:05 -0400

    Finish documentation of TellerEvent

--- PA5_tnguyen4/TellerEvent.cpp
+++ PA5_tnguyen4/TellerEvent.cpp
@@ -1,10 +1,25 @@
+/**
+ *
+ * TellerEvent.cpp
+ * @author Dung (Kevin) Nguyen
+ */
+
 #include "TellerEvent.h"
 
+/**
+ * @param t Time (in minutes) that this event happens
+ */
 TellerEvent::TellerEvent(double t, Teller *teller) : Event(t) {
 	this->teller = teller;
 }
 
-//TODO
+/**
+ * Pre-condition: size >= 1
+ * @param lines Array of lines in the bank
+ * @param size Number of lines in the bank
+ * @event Pointer to event queue
+ * Make this Teller Event act depending on the situation
+ */
 void TellerEvent::act(TellerQueue **lines, int size, EventQueue *eventQueue) {
 	if (this->teller->justServed()) {
 		// update service time
@@ -17,6 +32,9 @@ void TellerEvent::act(TellerQueue **lines, int size, EventQueue *eventQueue) {
 	this->teller->serveOrBreak(lines, size, eventQueue, this->getTime());
 }
 
+/**
+ * Destructor
+ */
 TellerEvent::~TellerEvent() {
-
+	// deliberately empty
 }
