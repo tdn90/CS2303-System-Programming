c1a61e8f4d1f1a23362212650367a6a910407382
tdn90 <tnguyen4@wpi.edu>
2018-10-11 20:07:26 -0400

    Complete documentation of CustomerEvent

--- PA5_tnguyen4/CustomerEvent.cpp
+++ PA5_tnguyen4/CustomerEvent.cpp
@@ -1,22 +1,33 @@
 #include "CustomerEvent.h"
 
+/**
+ * @param t Time (in minute) that this Customer event happens
+ * @param customer Pointer to the Customer arrived at the bank
+ */
 CustomerEvent::CustomerEvent(double t, Customer *customer) : Event(t) {
 	this->customer = customer;
 }
 
+/**
+ * Destructor
+ */
 CustomerEvent::~CustomerEvent() {
 	// do nothing here, we don't want to delete customer
 }
 
-//TODO
+/**
+ * Pre-condition: size >= 1
+ * @param lines Array of lines in the bank
+ * @param size Number of lines in the bank
+ * @event Pointer to event queue
+ * Make this Customer Event act depending on the situation
+ */
 void CustomerEvent::act(TellerQueue **lines, int size, EventQueue *eventQueue) {
 	if (this->customer->isDone()) { // Customer already get served
 		// collect statistic
 		this->customer->setEndTime(this->getTime());
 	}
-	else { // customer just arrived
-		// Customer get in line
-
+	else { // customer just arrived, get in line
 		// get line for customer
 		TellerQueue * shortestLine = getCustomerLine(lines, size);
 
@@ -36,6 +47,10 @@ void CustomerEvent::act(TellerQueue **lines, int size, EventQueue *eventQueue) {
 TellerQueue * CustomerEvent::getCustomerLine(TellerQueue **lines, int size) {
 	int min = lines[0]->size(); // set first line's length to minimum size
 	TellerQueue * shortestLine = lines[0];
+
+	/* i is current index in lines
+	 * Loop through lines to choose the shortest line
+	 */
 	for (int i = 1; i < size; i++) {
 		if (lines[i]->size() < min) {
 			min = lines[i]->size();
